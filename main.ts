473c57669ece1a174ec42749fbf1b0f4c3cbdd95
ajduro <44377619+ajduro@users.noreply.github.com>
2021-12-11 23:24:35 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(minTemp)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(maxTemp)
+})
+let minTemp = 0
+let maxTemp = 0
 let currenttemperature = input.temperature()
-let maxTemp = input.temperature()
-let minTemp = input.temperature()
+maxTemp = input.temperature()
+minTemp = input.temperature()
 basic.forever(function () {
     basic.showString(".")
     currenttemperature = input.temperature()
